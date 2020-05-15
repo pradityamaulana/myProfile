f577bf1c2a6db002717ff0205c91109c5de2654c
praditya maulana <you@example.com>
2020-05-15 22:02:24 +0700

    second commit

--- myProfile.php
+++ myProfile.php
@@ -3,3 +3,5 @@ echo '<br>';
 echo 'Nama : Praditya Maulana Yudistira';
 echo 'Kelas : XI RPL 1 <br>';
 echo 'Asal Sekolah : SMK Taruna Bhakti <br>';
+echo 'No HP: 085640821433 <br>';
+echo 'Email : pradityayudistira28@gmail.com <br>';
